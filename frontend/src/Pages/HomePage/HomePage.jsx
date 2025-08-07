cd977999ac73a6762ee6738809c5b15452e94ac7
Somay-kousis <somaykaush@gmail.com>
2025-08-07 17:06:15 +0530

    initials

---
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Background from './Background.jsx';
+import Header from './Header.jsx';
+import Menu from './Menu.jsx';
+
+export default function Homepage (){
+return (
+    <div>
+<Background/>
+<Header/>
+<Menu/>
+</div>
+)
+}
