673f339168d434709448dd79b3fed8f5eab7ebca
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-24 12:48:24 +0530

    token hashing function added

--- backend/src/models/temp.model.js
+++ backend/src/models/temp.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('cypto');
 
 const tempSchema = new mongoose.Schema({
 	email: {
@@ -15,6 +16,19 @@ const tempSchema = new mongoose.Schema({
 		type: Date
 	}
 });
+
+tempSchema.methods.getVerificationToken = function(){
+	const token = crypto.randomBytes(29).toString('hex');
+	this.mailToken = crypto
+	.createHash('sha256')
+	.update(token)
+	.digest('hex');
+
+	this.mailTokenExp = Date.now() + 30*60*1000;
+	return token;
+}
+
+
 
 
 
